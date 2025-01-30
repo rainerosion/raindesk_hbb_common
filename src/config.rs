52401e2b577376d8c2d7c3c0a829f136134ae5b8
rainerosion <lm@rainss.cn>
2025-01-30 20:15:06 +0800

    change RENDEZVOUS_SERVERS

--- src/config.rs
+++ src/config.rs
@@ -97,8 +97,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["hbbs.rustdesk.suger.live"];
+pub const RS_PUB_KEY: &str = "RBpKEUU0Sjr0+z41zqbaIPGF96HMVgdRzHyj4GoQE90=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
